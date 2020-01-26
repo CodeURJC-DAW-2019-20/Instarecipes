87482dab8bb4523ab20d7389674a6ff2467036cc
Daniel <sittingbout@gmail.com>
2020-01-26 20:18:21 +0100

    commit login

---
+++ Code/js/login.js
@@ -0,0 +1,14 @@
+
+function login()
+{
+document.getElementById("loginDiv").style.display = "block";
+document.getElementById("signupDiv").style.display = "none";
+
+}
+
+function signup()
+{
+    document.querySelector("loginDiv").style.display = "none";
+    document.querySelector("signupDiv").style.display = "block";
+
+}
