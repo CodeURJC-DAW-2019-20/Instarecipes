4f575d1bc98a54bd1e0500c040e077dd5d444301
Hamsi <genioinstrumental@gmail.com>
2020-02-02 04:16:30 +0100

    Revert "Revert "Merge branch 'login'""
    This reverts commit 81ec28402544fdaa195404b085317289b3953484.

---
+++ Code/js/profile.js
@@ -0,0 +1,7 @@
+var completeProfile = document.getElementById('completeProfile');
+var divCompleteProfile = document.getElementById('completeProfileDiv');
+
+completeProfile.onclick = function(){
+    divCompleteProfile.style.display="block";
+   return false;
+}
