e8f691fe87728957f8e4074d0f27c23d615057a3
Hamsi <genioinstrumental@gmail.com>
2020-02-16 22:39:53 +0100

    Added from merge: in description
    - Reordered folders
    - Database entities and initialize examples
    - Login user
    - Register user and autologin
    - Security links
    - Error pages
    In progress: search filtered page

--- backend/src/main/java/com/proyect/instarecipes/users/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyect.instarecipes.users;
-
-import com.proyect.instarecipes.users.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<User, Long> {
-    // List<User> findByLastName(String lastName);
-    // List<User> findByFirstName(String firstName);
-}
