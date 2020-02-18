b480bb33471516f4d4baafa3cfefd0d74c375fbf
Daniel <sittingbout@gmail.com>
2020-02-18 19:50:50 +0100

    upd load-recipe.js

--- backend/src/main/resources/static/js/load-recipe.js
+++ backend/src/main/resources/static/js/load-recipe.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $("publications-item mx-auto img").click(function(){
-        $("recipes-container").load("recipe-container.html #more-recipes");
+    $("#load-recipe-btn a").click(function(){
+        $("#recipes-container").load("recipe-container.html #more-recipes");
         return false;
     });
 
