bb79574f772eaa1fde8f5bc4b79ed7b33bf94dfa
Daniel <sittingbout@gmail.com>
2020-02-23 20:45:01 +0100

    validated email, error msg/ some changes

--- backend/src/main/resources/static/js/login.js
+++ backend/src/main/resources/static/js/login.js
@@ -1,8 +1,19 @@
 var signUp = document.getElementById('signUp');
 var divSignUp = document.getElementById('signUpDiv');
 var divCompleteSignUp = document.getElementById('completeSignUpDiv');
+var emailInfo = document.getElementById('email');
+var emailMsg = document.getElementById('email-val');
 signUp.onclick = function(){
-    divCompleteSignUp.style.display = "block";
-    divSignUp.style.display = "none";
-    return false;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (emailInfo.value.match(mailformat)) {
+        divCompleteSignUp.style.display = "block";
+        divSignUp.style.display = "none";
+        return true;
+    }
+    else {
+        emailMsg.style.display = "block";
+        return false;
+    }
+    
+    
 }
