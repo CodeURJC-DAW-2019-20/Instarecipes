7e21291806210e852c4f9dc32d8c10102de130d2
Daniel <sittingbout@gmail.com>
2020-02-23 23:55:17 +0100

    added password match w/ confirm password

--- backend/src/main/resources/static/js/login.js
+++ backend/src/main/resources/static/js/login.js
@@ -3,17 +3,30 @@ var divSignUp = document.getElementById('signUpDiv');
 var divCompleteSignUp = document.getElementById('completeSignUpDiv');
 var emailInfo = document.getElementById('email');
 var emailMsg = document.getElementById('email-val');
-signUp.onclick = function(){
+var pass = document.getElementById('pass');
+var cpass = document.getElementById('cpass');
+var divPass = document.getElementById('conf-pass');
+signUp.onclick = function () {
+
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (emailInfo.value.match(mailformat)) {
+
+    if (emailInfo.value.match(mailformat) && pass.value.match(cpass.value)) {
         divCompleteSignUp.style.display = "block";
         divSignUp.style.display = "none";
-        
+        alert("la password coincide");
+
     }
-    else {
+    else if (!pass.value.match(cpass.value)) {
+        divPass.style.display = "block";
+
+    }
+    else if (!emailInfo.value.match(mailformat)) {
         emailMsg.style.display = "block";
-        
+
     }
-    
-    
+
+    return false;
+
+
 }
+
