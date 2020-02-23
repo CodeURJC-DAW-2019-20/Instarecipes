29d80824dae48826cd70510d96e4cfd6552fa95f
Muffinous <37084155+Muffinous@users.noreply.github.com>
2020-02-23 20:48:20 +0100

    I just want lordkener commit.

--- backend/src/main/java/com/proyect/instarecipes/models/Comment.java
+++ backend/src/main/java/com/proyect/instarecipes/models/Comment.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 @Entity
 public class Comment{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
--- backend/src/main/java/com/proyect/instarecipes/models/CookingStyle.java
+++ backend/src/main/java/com/proyect/instarecipes/models/CookingStyle.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class CookingStyle{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String cookingStyle;
