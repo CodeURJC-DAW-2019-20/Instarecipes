9b2a681303ad62eeb958dfef55d5959137bc358d
Hamsi <genioinstrumental@gmail.com>
2020-02-28 19:25:04 +0100

    comments in process 80%

--- backend/src/main/java/com/proyect/instarecipes/controllers/RecipePageController.java
+++ backend/src/main/java/com/proyect/instarecipes/controllers/RecipePageController.java
@@ -4,6 +4,7 @@ import com.proyect.instarecipes.repositories.RecipesRepository;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -55,7 +56,7 @@ public class RecipePageController {
         model.addAttribute("n_publications", pubs);
         model.addAttribute("n_likes", likes);
 
-        // // Recipe
+        // Recipe
         Recipe recipe = recipesRepository.findRecipeById(id);
         model.addAttribute("recipe", recipe);
         model.addAttribute("id", recipe.getId());
@@ -67,8 +68,25 @@ public class RecipePageController {
 
         // Comments
         List<Comment> comments = commentsRepository.findAllByRecipe(recipe);
+        System.out.println("Comentarios: " + comments.size());
         model.addAttribute("n_comments", comments.size());
+        
+        User actual = userSession.getLoggedUser();
+        if(actual!=null){
+            for(int i=0;i<comments.size();i++){ //algoritm to check if the actual use liked that comment
+                boolean aux = false;
+                for(User uAux : comments.get(i).getUsersLiked()){
+                    if(uAux.getId() == actual.getId()){
+                        System.out.println("SOY IGUAL");
+                        aux = true;
+                    }
+                }
+                comments.get(i).setLiked(aux);
+            }
+        }
+        
         model.addAttribute("comments", comments);
+
         return "recipe";
     }
 
@@ -76,31 +94,69 @@ public class RecipePageController {
     public void postComment(@PathVariable Long id, Model model, @RequestParam String content, HttpServletResponse response,
             @RequestParam(required = false, value = "parentComment") Long parentComment) throws IOException {
         
-        GroupStaff groupStaff = new GroupStaff();
-        Optional<Comment> pComment = null; //The parent comment
-        Set<Comment> subComment = null; //The subcomment
         Comment comment = null;
-
         User u = userSession.getLoggedUser();
         Recipe r = recipesRepository.findRecipeById(id);
         
-        if(parentComment != null){
-            pComment = commentsRepository.findById(parentComment);
-        }
-        if((content != null)){
-            if(pComment == null){
-                comment = new Comment(u, content, null, 0, r, false, false);
-            }else{
-                subComment = groupStaff.groupComments(pComment.get());
-                comment = new Comment(u, content, subComment, 0, r, false, true);
-            }
+        if(parentComment != null){ //Subcomment
+            Optional<Comment> pComment = commentsRepository.findById(parentComment);
+            comment = new Comment(u, content, null, r, false, true, null);//get the comment
             if(content != ""){
                 commentsRepository.save(comment);
+                Set<Comment> ejem = new HashSet<>();
+                ejem = pComment.get().getSubComments();
+                System.out.println("Precom: " + ejem);
+                ejem.add(comment);
+                System.out.println("Postcom: " + ejem);
+                if(!pComment.get().isSubcomment()){
+                    commentsRepository.setParentHasComment(true, parentComment);
+                }
+                else{
+                    commentsRepository.setParentHasComment(false, parentComment);
+                }
+            }
+        }else{ //Normal comment
+            comment = new Comment(u, content, null, r, false, false, null);
+            if(content != ""){
+                commentsRepository.save(comment);
+                System.out.println("Comentario actualizado");
             }
         }
         response.sendRedirect("../recipes/"+id);
     }
+    
+    @PostMapping("/likeComment/{id}")
+    public void likeComment(@PathVariable Long id, Model model, @RequestParam String content, HttpServletResponse response,
+            @RequestParam(required = false, value = "parentComment") Long parentComment) throws IOException {
+        
+        Comment comment = null;
+        User u = userSession.getLoggedUser();
+        Recipe r = recipesRepository.findRecipeById(id);
+        
+        
+        response.sendRedirect("../recipes/"+id);
+    }
 
+    @PostMapping("/unlikeComment/{id}")
+    public void unlikeComment(@PathVariable Long id, @RequestParam Long id_recipe, HttpServletResponse response) throws IOException {
+        
+        Optional<Comment> comment = commentsRepository.findById(id);
+        User actual = userSession.getLoggedUser();
+
+        for(User uAux : comment.get().getUsersLiked()){
+            if(uAux.getId() == actual.getId()){
+                comment.get().setLiked(false);
+                System.out.println("Eliminado id: " + comment.get().getId());
+                System.out.println("Size 1: " + comment.get().getUsersLiked().size());
+                comment.get().removeLikeUser(actual);
+                // commentsRepository.deleteLikeComment()
+                System.out.println("Size 2: " + comment.get().getUsersLiked().size());
+                break;
+            }
+        }
+        
+        response.sendRedirect("../recipes/"+id_recipe);
+    }
 
     @ModelAttribute
 	public void addAttributes(Model model) {
--- backend/src/main/java/com/proyect/instarecipes/repositories/CommentsRepository.java
+++ backend/src/main/java/com/proyect/instarecipes/repositories/CommentsRepository.java
@@ -1,18 +1,41 @@
 package com.proyect.instarecipes.repositories;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import com.proyect.instarecipes.models.Comment;
 import com.proyect.instarecipes.models.Recipe;
+import com.proyect.instarecipes.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface CommentsRepository extends JpaRepository<Comment, Long> {
     //The querys has to be so strict, every single letter should go as same as the classes, like capital letters
     @Query("SELECT COUNT(*) FROM Comment c WHERE c.recipe= :id_recipe") 
     int countByRecipeId(Recipe id_recipe);
 
-	List<Comment> findAllByRecipe(Recipe recipe);
+    List<Comment> findAllByRecipe(Recipe recipe);
+    
+    // @Transactional
+    // @Modifying
+    // @Query("UPDATE Comment c SET c.subComments = :new_list WHERE c.id = :cid")
+	// void newSubcomments(@Param("new_list") Set<Comment> new_list, @Param("cid") Long cid);
+
+    @Query("SELECT c FROM Comment c WHERE c.id = :cid")
+    Set<Comment> findSubCommentsById(Long cid);
+    
+    @Transactional
+    @Modifying
+    @Query("UPDATE Comment c SET c.hasSubcomments = :now_hasComment WHERE c.id = :cid")
+    void setParentHasComment(@Param("now_hasComment") boolean now_hasComment, @Param("cid") Long cid);
+
+    @Query("SELECT cl.usersLiked FROM Comment cl WHERE cl.id = :id")
+	Set<User> findLikeUsersList(Long id);
+
 }
