ee41238ba5f17aa8475a3d62d7310e38b29c210f
Muffinous <37084155+Muffinous@users.noreply.github.com>
2020-03-06 01:57:32 +0100

    Search filtered Api!
    Im gonna commit para aprovecharme de manu jeje

--- backend/src/main/java/com/proyect/instarecipes/api/SearchPageRestController.java
+++ backend/src/main/java/com/proyect/instarecipes/api/SearchPageRestController.java
@@ -1,15 +1,15 @@
 package com.proyect.instarecipes.api;
-import java.util.Optional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.proyect.instarecipes.models.Recipe;
@@ -19,7 +19,7 @@ import com.proyect.instarecipes.models.Category;
 import com.proyect.instarecipes.models.Allergen;
 import com.proyect.instarecipes.models.CookingStyle;
 
-import com.proyect.instarecipes.repositories.RecipesRepository;
+import com.proyect.instarecipes.service.SearchService;
 
 @RestController
 @RequestMapping("/api/search")
@@ -28,22 +28,58 @@ public class SearchPageRestController {
 	public interface SearchInfo extends Recipe.RecipeBasic, Recipe.RecipeView, Recipe.RecipeACS, User.Username, User.NameSurname,
 	 Ingredient.Item, Category.Item, Allergen.Item, CookingStyle.Item{}
 
-    
-    @Autowired
-	private RecipesRepository recipesRepository;
+	@Autowired
+	private SearchService searchService;
+
 	
     @JsonView(SearchPageRestController.SearchInfo.class)
-	@GetMapping("/")
-	public ResponseEntity<Recipe> getItem(@RequestParam long id) {
+	@PostMapping("/filtered")
+	public ResponseEntity<List<Recipe>> getFilteredRecipes(@RequestParam(required = false) String ingredients, 
+    @RequestParam(required = false) String categories, @RequestParam(required = false) 
+    String cookingStyles, @RequestParam(required = false) String allergens) {
+		
+		ArrayList<String> cookingStylesSelected = searchService.getItem(cookingStyles);
+		ArrayList<String> categoriesSelected = searchService.getItem(categories);
+        ArrayList<String> allergensSelected = searchService.getItem(allergens);
+        ArrayList<String> ingredientsSelected = searchService.getItem(ingredients);
 
-		Optional<Recipe> recipe = recipesRepository.findById(id);
+		// List<Category> restOfCategories = searchService.restCategories(searchService.getAllCategories(), categoriesSelected);
+        // List<CookingStyle> restOfCookingStyles = searchService.restCookingStyles(searchService.getAllCookingStyles(), cookingStylesSelected);
+        // List<Ingredient> restOfIngredients = searchService.restIngredients(searchService.getAllIngredients(), ingredientsSelected);
+        // List<Allergen> restOfAllergens = searchService.restAllergens(searchService.getAllAllergens(), allergensSelected);
 
-		if (recipe.get() != null) {
-			return new ResponseEntity<>(recipe.get(), HttpStatus.OK);
+		List<Recipe> recipesFounded = searchService.getFilteredRecipes(ingredientsSelected, categoriesSelected, cookingStylesSelected, allergensSelected);
+		if (recipesFounded != null) {
+			return new ResponseEntity<>(recipesFounded, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
+	}
 
 
+    @JsonView(SearchPageRestController.SearchInfo.class)
+	@PostMapping("/navbar/users")
+	public ResponseEntity<List<User>> getUserSearch(@RequestParam(required = false) String search){
+		String firstLetter = search.substring(0,1);
+
+            if (firstLetter.equals("@")){
+				List<User> trueUsers = searchService.getTrueUsers(search);
+				return new ResponseEntity<>(trueUsers, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+	}
+
+	@JsonView(SearchPageRestController.SearchInfo.class)
+	@PostMapping("/navbar/recipes") 
+	public ResponseEntity<List<Recipe>> getRecipeSearch(@RequestParam(required = false) String search){
+		String firstLetter = search.substring(0,1);
+
+		if (!firstLetter.equals("@")){
+			List<Recipe> trueRecipes = searchService.getTrueRecipes(search);
+			return new ResponseEntity<>(trueRecipes, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 }
