7b304686db1faae068832bbe499bebd061354cdb
Hamsi <genioinstrumental@gmail.com>
2020-03-08 18:44:45 +0100

    Index 100% and postman 100%

--- backend/src/main/java/com/proyect/instarecipes/api/LogInRestController.java
+++ backend/src/main/java/com/proyect/instarecipes/api/LogInRestController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.proyect.instarecipes.models.User;
-import com.proyect.instarecipes.repositories.UsersRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,25 +22,24 @@ public class LogInRestController {
 
 	@Autowired
 	private UserSession userComponent;
-	@Autowired
-	private UsersRepository usersRepository;
 	private static final Logger log = LoggerFactory.getLogger(LogInRestController.class);
 
 	@JsonView(LogInRestController.ShowUser.class)
     @RequestMapping("/api/login")
 	public ResponseEntity<User> logIn() {
-		if (userComponent.isLoggedUser()) {
+		if (!userComponent.isLoggedUser()) {
 			log.info("Not user logged");
 			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 		}else{
 			User loggedUser = userComponent.getLoggedUser();
-			User u = usersRepository.findByUsername(loggedUser.getUsername());
+			// User u = usersRepository.findByUsername(loggedUser.getUsername());
 			log.info("Logged as " + loggedUser.getName());
 			log.info("My followers "+ loggedUser.getFollowersNum());
 			log.info("I am following "+ loggedUser.getFollowingNum());
-			return new ResponseEntity<>(u, HttpStatus.OK);
+			return new ResponseEntity<>(loggedUser, HttpStatus.OK);
 		}
 	}
+	
 	@RequestMapping("/api/logout")
 	public ResponseEntity<Boolean> logOut(HttpSession session) {
 		if (!userComponent.isLoggedUser()) {
