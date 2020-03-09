a832e581f5683948f697739674a641b513792ab8
Manuel Leon <manuellb1997@gmail.com>
2020-03-09 21:07:38 +0100

    Hi developers Now u can upload a image for ur avatar, i've been refactoring some files

--- backend/src/main/java/com/proyect/instarecipes/api/SignUpRestController.java
+++ backend/src/main/java/com/proyect/instarecipes/api/SignUpRestController.java
@@ -1,5 +1,8 @@
 package com.proyect.instarecipes.api;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.proyect.instarecipes.models.User;
 import com.proyect.instarecipes.repositories.UsersRepository;
 import com.proyect.instarecipes.security.ImageService;
+import com.proyect.instarecipes.security.UserSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,33 +32,41 @@ public class SignUpRestController{
     UsersRepository usersRepository;
     @Autowired
     private ImageService imageService;
+    @Autowired 
+    private UserSession usersession;
 
     @JsonView(SignUpRestController.showNewUser.class)
-    @RequestMapping("/signUp")
-    public ResponseEntity<User> SignUp(@RequestParam String username,@RequestParam String email, @RequestParam String password,@RequestParam  String name,
-    @RequestParam String surname,@RequestParam String description, @RequestParam String allergen, @RequestParam  MultipartFile avatar)throws IOException{ 
+    @RequestMapping("/signup")
+    public ResponseEntity<User> SignUp(@RequestBody User user1)throws IOException{ 
         ArrayList<User> users = new ArrayList<User>(usersRepository.findAll());
         boolean exist = false;
         Set<User> follow = new HashSet<>();
-        User user = new User(username, email, password, name, surname, description, allergen, follow, follow, "ROLE_USER");
+        System.out.println(user1.getUsername());
         for(User u : users){
-            if(u.getUsername().equalsIgnoreCase(user.getUsername())){
+            if(u.getUsername().equalsIgnoreCase(user1.getUsername())){
                 exist = true;
                 break;
             }
         }       
         if(!exist){
-            usersRepository.save(user);
-            if(!avatar.isEmpty()){
-                user.setAvatar(true);
-                usersRepository.flush();
-                imageService.saveImage("avatars", user.getId(), avatar);
-                return new ResponseEntity<>(user,HttpStatus.OK);
-            } else{
-                return new ResponseEntity<>(HttpStatus.CONFLICT);
-            }
+            User user = new User(user1.getUsername(), user1.getEmail(),user1.getPassword(),user1.getName(),user1.getSurname(), user1.getInfo(), user1.getAllergens(), follow, follow, "ROLE_USER");
+            usersRepository.save(user);          
+            return new ResponseEntity<>(user,HttpStatus.OK);
         } else{
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
-    }    
+    }
+    @JsonView(SignUpRestController.showNewUser.class)
+    @PostMapping("/signup/image") 
+    public ResponseEntity<User> uploadImage(@RequestParam MultipartFile avatar) throws IOException{
+    if(usersession.isLoggedUser()){
+        User user =usersession.getLoggedUser();
+        user.setAvatar(true);
+        imageService.saveImage("avatars", user.getId(), avatar);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.NETWORK_AUTHENTICATION_REQUIRED);
+
+    }
+
 }
