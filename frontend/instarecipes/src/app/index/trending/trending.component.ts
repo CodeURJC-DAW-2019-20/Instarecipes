8ad57ea2293df358978066575b908e6d028cc9f4
Hamsi <genioinstrumental@gmail.com>
2020-04-01 19:15:11 +0200

    Trending done, less add recipe and filter recipe

--- frontend/instarecipes/src/app/index/trending/trending.component.ts
+++ frontend/instarecipes/src/app/index/trending/trending.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChildren, AfterViewInit, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Recipe } from 'src/app/Interfaces/recipe.model';
 import { RecipesService } from 'src/app/services/recipes.service';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -8,30 +8,16 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './trending.component.html',
   styleUrls: ['./trending.component.css']
 })
-export class TrendingComponent implements AfterViewInit, OnInit{
+export class TrendingComponent implements OnInit{
 
     trendingRecipes: Recipe[] = [];
     trendingElement: any[] = [];
     avatar: any[] = [];
     image: any[] = [];
-
-    @ViewChildren('container') container: { toArray: () => { nativeElement: any; }[]; };
+    loadingTrending: boolean = true;
 
     constructor(private recipesService: RecipesService, 
       private domSanitizer: DomSanitizer){ }
-
-    ngAfterViewInit() {
-      this.trendingRecipes.forEach(element => {
-        this.userRecipeAvatar(element);
-        this.recipeStepImage(element, 1);
-      });
-      this.container.toArray().forEach((element,index) => {
-        this.trendingElement.push(element.nativeElement.querySelector("div"));
-        if (index > 0){
-          this.trendingElement[index].classList.remove("active");
-        }
-      });
-    }
 
     ngOnInit(){
       this.refreshTrending();
@@ -41,14 +27,13 @@ export class TrendingComponent implements AfterViewInit, OnInit{
       this.recipesService.getIndexTrendingRecipes().subscribe(
         recipes => {
           this.trendingRecipes = recipes as Recipe[];
+          this.loadingTrending = false;
+          this.trendingRecipes.forEach(element => {
+            this.userRecipeAvatar(element);
+            this.recipeStepImage(element, 1);
+          });
         }
       );
-      this.container.toArray().forEach((element,index) => {
-        this.trendingElement.push(element.nativeElement.querySelector("div"));
-        if (index > 0){
-          this.trendingElement[index].classList.remove("active");
-        }
-      });
     }
 
     userRecipeAvatar(r: Recipe) {
