3a2a912d299dfa2e7701fbea2184734e08e52d7e
trevod98 <josemariamelerogimenez98@hotmail.com>
2020-04-17 14:03:39 +0200

    items and users

--- frontend/instarecipes/src/app/profile/items/items.component.ts
+++ frontend/instarecipes/src/app/profile/items/items.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { EditProfileComponent } from '../popup/editProfile/editProfile.component';
+import {Component,OnInit} from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { ProfileService } from '../../services/profile.service';
+import { Router } from '@angular/router';
+import { User } from '../../Interfaces/user.model';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RecipesService } from '../../services/recipes.service';
+import { Recipe } from '../../Interfaces/recipe.model';
+import { isNumber } from 'util';
+//import { EditProfileComponent } from '../../popup/editProfile/editProfile.component';
 //import { MatDialog, MatDialogConfig } from "@angular/material";
 
 @Component({
@@ -8,13 +16,71 @@ import { EditProfileComponent } from '../popup/editProfile/editProfile.component
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+  users: User[] = []
+  image: any[] = [];
+  user: User;
+  recipes: any[]=[];
+  href: string = "";
+  parts: string[]=[];
+  id_user: number=-1;
+  constructor(private profileService: ProfileService,private recipesService: RecipesService,
+    private domSanitizer: DomSanitizer,private router: Router){
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
-  editProfile(){
-    //this.dialog.open(EditProfileComponent);
+  ngOnInit(){
+    this.href = this.router.url;
+    this.parts=this.href.split('/');
+    if(isNumber( this.parts[-1])){
+      this.id_user=Number(this.parts[-1]);
+      this.refresh(this.id_user);
+    }
+    else{
+      this.refresh1();
+
+    }
   }
+
+  refresh(id_user : number){
+    this.profileService.getUser(id_user).subscribe(
+      user => {
+        this.user = user;
+        this.get_Recipes(id_user);
+        for(var recipe of this.recipes){
+          this.recipeMainImage(recipe);
+        }     }
+    );
+  }
+  refresh1(){
+    this.profileService.getActualUser().subscribe(
+      user => {
+        this.user = user;
+        this.get_Recipes(this.id_user);
+        for(var recipe of this.recipes){
+          this.recipeMainImage(recipe);
+        }   
+      }
+    )
+  }
+  get_Recipes(id_user: number){
+    this.profileService.getProfileAvatar(id_user).subscribe(
+      data => {
+        var urlCreator = window.URL;
+        this.recipes.push(this.domSanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(data)));
+      }
+    );
+  }
+  
+  recipeMainImage(r: Recipe) {
+    this.recipesService.getRecipeAvatar(r.username.id).subscribe(
+      data => {
+        var urlCreator = window.URL;
+        this.image.push(this.domSanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(data)));
+      }
+    );
+    
+  }
+  
+
+  
 }
--- frontend/instarecipes/src/app/profile/users/users.component.ts
+++ frontend/instarecipes/src/app/profile/users/users.component.ts
@@ -1,21 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../services/user.service';
+import { ProfileService } from '../../services/profile.service';
+import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 import { User } from '../../Interfaces/user.model';
-import { AuthenticationService } from 'src/app/services/authentication.service';
+import { isNumber } from 'util';
 
 @Component({
-  selector: 'user-profile',
+  selector: 'user-info',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: User[] = []
+  avatar: any[] = [];
   user: User;
-  a = history.state.data.a;
-  constructor(public authService: AuthenticationService) { }
+  href: string = "";
+  parts: string[]=[];
+  user_actual: User;
+  id_user: number=-1;
+  constructor(private profileService: ProfileService, 
+    private domSanitizer: DomSanitizer,private router: Router){
 
-  ngOnInit() {
-    console.log('heeeeeeeello ', this.a);
   }
+
+  ngOnInit(){
+    this.href = this.router.url;
+    this.parts=this.href.split('/');
+    if(isNumber( this.parts[-1])){
+      this.id_user=Number(this.parts[-1]);
+      this.refresh(this.id_user);
+    }
+    else{
+      this.refresh1();
+
+    }
+    
+  }
+
+  refresh(id_user : number){
+    this.profileService.getUser(id_user).subscribe(
+      user => {
+        this.user = user;
+        this.userProfileAvatar(id_user);    }
+    );
+  }
+  
+  refresh1(){
+    this.profileService.getActualUser().subscribe(
+      user => {
+        this.user = user;
+        this.userProfileAvatar(user);
+      }
+    )
+  }
+
+  userProfileAvatar(u: number) {
+    
+    this.profileService.getProfileAvatar(u).subscribe(
+      data => {
+        var urlCreator = window.URL;
+        this.avatar.push(this.domSanitizer.bypassSecurityTrustUrl(urlCreator.createObjectURL(data)));
+      }
+    );
+  }
+  
 
 }
